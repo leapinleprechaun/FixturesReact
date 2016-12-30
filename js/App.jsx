b0b9b4b61d3916877d313b94b756bcc31e5bf901
Gill <leapin_leprechaun@hotmail.com>
2016-12-30 12:47:08 +0000

    initial setup

---
+++ js/App.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const App = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+       test
+      </div>
+    )
+  }
+})
+
+module.exports = App
---
+++ test/App.spec.jsx
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+const {expect} = require('chai')
+const React = require('react')
+
+xdescribe("test", () => {
+  it('tests', () => {
+  })
+
+})
