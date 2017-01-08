6ca0aed9fc939e3cbae5002bc8037366a445fc31
Gill <leapin_leprechaun@hotmail.com>
2017-01-08 19:34:37 +0000

    added component for dealing with chossing a club

---
+++ js/ChooseAClub.jsx
@@ -0,0 +1,10 @@
+const React = require('react')
+const Heading = require('./Heading')
+
+const ChooseAClub = React.createClass({
+  render () {
+    return <Heading heading='Choose a Club' />
+  }
+})
+
+module.exports = ChooseAClub
