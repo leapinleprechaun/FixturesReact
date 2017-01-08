5ad44341c2c5adf13d75d74d3176d1d325ac6af9
Gill <leapin_leprechaun@hotmail.com>
2017-01-08 19:36:53 +0000

    Router with routes for index, club and gender set up

--- js/App.jsx
+++ js/App.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
+const Layout = require('./Layout')
+const ChooseAClub = require('./ChooseAClub')
+const SelectedClubChooseGender = require('./SelectedClubChooseGender')
+const SelectedGenderShowMatches = require('./SelectedGenderShowMatches')
+const {Route, Router, IndexRoute, hashHistory} = require('react-router')
+          // <Route path='/search' component={Search} />
 
 const App = React.createClass({
   render () {
     return (
-      <div className='container'>
-       test
-      </div>
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout} >
+          <IndexRoute component={ChooseAClub} />
+          <Route path='/club/:club' component={SelectedClubChooseGender} />
+          <Route path='/club/:club/gender/:gender' component={SelectedGenderShowMatches} />
+        </Route>
+      </Router>
     )
   }
 })
